311c8199aa9abe137afb6a9873f9ea209efd37b3
schmids <s.schmid@techdivision.com>
2014-02-26 18:19:42 +0100

    FILE: index.html && add login.js
    File
    ==
    Login mit Session erfolgreich eingebaut.
    Login.js enthält 2 neue Funktionen.

---
+++ login.js
@@ -0,0 +1,29 @@
+
+		  	/* Check, if the user already logged in */
+			function init(){
+				if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined){
+					$("#username").val(window.localStorage["username"]);
+					$("#password").val(window.localStorage["password"]);
+					check();
+				}
+				
+			}
+			/* Check, if uname = Username && upass = password */
+			function check()/*function to check userid & password*/
+			{
+			var u = $("#username").val();
+			var p = $("#password").val();
+			console.log(u);
+			console.log(p);
+			 /*the following code checkes whether the entered userid and password are matching*/
+			 if(form.uname.value == "admin" && form.upass.value == "admin")
+			  {
+				window.localStorage["username"] = u;
+                window.localStorage["password"] = p;
+				window.location.href = 'main.html';/*opens the target page while Id & password matches*/
+			  }
+			 else
+			 {
+				 navigator.notification.alert("Du hast dich mit den falschen Daten angemeldet!", function() {});
+			  }
+			}
